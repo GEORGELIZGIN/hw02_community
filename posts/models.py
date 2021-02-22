228a349c8e9da7435dfcb73547191debab5210b8
Georgiy <lizgin.gv@phystech.edu>
2021-02-22 04:06:45 +0300

    commit

--- posts/models.py
+++ posts/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     description = models.TextField(blank=True)
 
     def __str__(self):

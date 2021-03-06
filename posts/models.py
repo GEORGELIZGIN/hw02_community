4433d8418f76285a1482fad989317950b8b0eb24
Georgiy <lizgin.gv@phystech.edu>
2021-03-06 21:28:41 +0300

    commit

--- posts/models.py
+++ posts/models.py
@@ -8,7 +8,13 @@ class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
-    group = models.ForeignKey('Group', on_delete=models.CASCADE, blank=True, null=True)
+    group = models.ForeignKey('Group', on_delete=models.CASCADE, blank=True, null=True, related_name='posts')
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        ordering = ("-pub_date",)
 
 
 class Group(models.Model):

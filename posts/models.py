9350bacc5bbfe86e8f9a8a77450d7b23a2b42fb2
Georgiy <lizgin.gv@phystech.edu>
2021-03-06 12:36:46 +0300

    commit

--- posts/models.py
+++ posts/models.py
@@ -7,7 +7,8 @@ User = get_user_model()
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('date published', auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="posts")
     group = models.ForeignKey('Group', on_delete=models.CASCADE,
                               blank=True, null=True, related_name='posts')
 
